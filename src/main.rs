5a7003d33cb8f50b9fae74c1ffd1bb999938db7a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-07-08 21:23:30 +0200

    Simplify the if-let statement

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use config::Config;
 fn main() {
     println!("ksv web server");
 
-    if let Err(e) = HttpServer::new(Config::default()).serve() {
-        panic!("{}", e.to_string());
+    let server = HttpServer::new(Config::default()); 
+
+    if let Err(e) = server.serve() {
+        panic!("{}", e);
     }
 }
