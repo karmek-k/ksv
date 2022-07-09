20dd213897c7eda7087f36bc8c41f01c37dff8fd
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-07-09 11:41:59 +0200

    Use an unpriviledged port to avoid permission errors

--- src/config.rs
+++ src/config.rs
@@ -11,7 +11,7 @@ impl Config {
     pub fn default() -> Self {
         Self { 
             address: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            port: 80_u16,
+            port: 8000_u16,
             root: PathBuf::from("~/ksv-root"),
         }
     }
