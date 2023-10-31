66e3f0f8784c96f8ad844e8b4082d30329ed2b99
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-10-31 10:54:13 +0100

    feat(request): parse request method and location

--- src/http/request.rs
+++ src/http/request.rs
@@ -9,8 +9,28 @@ pub fn parse(stream: &mut dyn Read) -> HttpRequest {
     let mut buffer = [0; 2048];
     stream.read(&mut buffer).unwrap();
 
+    // read while splitting by spaces
+    let space_code: u8 = 32;
+
+    let mut strings: Vec<String> = vec![];
+    let mut string = String::new();
+    for i in buffer {
+        if i == space_code {
+            strings.push(string.clone());
+            string = String::new();
+            continue;
+        }
+
+        string.push(i.into());
+    }
+    // identify parsed characters as method name
+
+    // check method name (only letters)
+
+    println!("{:?}", strings);
+
     HttpRequest {
-        method: String::new(),
-        location: String::new(),
+        method: strings[0].clone(),
+        location: strings[1].clone(),
     }
 }
