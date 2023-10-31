bd2ffab2becc9efd04f7985cb9f2167e381f8dc9
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-10-31 11:03:18 +0100

    feat: put `HttpRequest` functions in impl

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,36 +1,40 @@
 use std::io::Read;
 
+/// A HTTP request abstraction.
 pub struct HttpRequest {
     pub method: String,
     pub location: String,
 }
 
-pub fn parse(stream: &mut dyn Read) -> HttpRequest {
-    let mut buffer = [0; 2048];
-    stream.read(&mut buffer).unwrap();
+impl HttpRequest {
+    /// Construct a `HttpRequest` from a stream of bytes.
+    pub fn parse_stream(stream: &mut dyn Read) -> Self {
+        let mut buffer = [0; 2048];
+        stream.read(&mut buffer).unwrap();
 
-    // read while splitting by spaces
-    let space_code: u8 = 32;
+        // read while splitting by spaces
+        let space_code: u8 = 32;
 
-    let mut strings: Vec<String> = vec![];
-    let mut string = String::new();
-    for i in buffer {
-        if i == space_code {
-            strings.push(string.clone());
-            string = String::new();
-            continue;
+        let mut strings: Vec<String> = vec![];
+        let mut string = String::new();
+        for i in buffer {
+            if i == space_code {
+                strings.push(string.clone());
+                string = String::new();
+                continue;
+            }
+
+            string.push(i.into());
         }
+        // identify parsed characters as method name
 
-        string.push(i.into());
-    }
-    // identify parsed characters as method name
+        // check method name (only letters)
 
-    // check method name (only letters)
+        println!("{:?}", strings);
 
-    println!("{:?}", strings);
-
-    HttpRequest {
-        method: strings[0].clone(),
-        location: strings[1].clone(),
+        HttpRequest {
+            method: strings[0].clone(),
+            location: strings[1].clone(),
+        }
     }
 }
--- src/http/request_test.rs
+++ src/http/request_test.rs
@@ -1,13 +1,13 @@
 #[cfg(test)]
 mod tests {
-    use crate::http::request::parse;
+    use crate::http::request::HttpRequest;
 
     #[test]
     fn test_parse_request() {
         let request = String::from("GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n");
         let mut stream = request.as_bytes();
 
-        let request = parse(&mut stream);
+        let request = HttpRequest::parse_stream(&mut stream);
 
         assert_eq!(request.method, "GET");
         assert_eq!(request.location, "/");
