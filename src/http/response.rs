f196ebc9cd863d8106844aa587482a733e96cf26
karmek-k <33288445+karmek-k@users.noreply.github.com>
2025-12-27 18:06:44 +0100

    refactor(response): docstrings

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,16 +1,17 @@
 use super::status::Status;
 
-/// A HTTP response abstraction.
-///
-/// Implements the `ToString` trait, `to_string` returns
-/// a valid HTTP response that can be sent back to the requester.
+/// HTTP response abstraction.
 pub struct HttpResponse<'a> {
+    /// Status code
     pub status: Status,
+    /// Content type of the response
     pub content_type: &'a str,
+    /// Response body
     pub body: String,
 }
 
 impl<'a> ToString for HttpResponse<'a> {
+    /// Returns the HTTP response as a string.
     fn to_string(&self) -> String {
         let (code, status_msg) = self.status.tuple();
         let extra_length = 4;
@@ -27,6 +28,7 @@ impl<'a> ToString for HttpResponse<'a> {
 }
 
 impl<'a> Default for HttpResponse<'a> {
+    /// Returns a response with code 200, of type `text/plain` and with empty body.
     fn default() -> Self {
         Self {
             status: Status::Ok,
