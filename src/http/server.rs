77e8546deeb9b7f3058309ee077ea0c1aed83875
karmek-k <33288445+karmek-k@users.noreply.github.com>
2025-12-27 18:35:13 +0100

    refactor(server): comments

--- src/http/server.rs
+++ src/http/server.rs
@@ -9,22 +9,20 @@ use crate::http::status::Status;
 
 use log::{error, info};
 
-/// This is the base HTTP server that powers ksv.
+/// Base HTTP server.
 pub struct HttpServer {
+    /// Server configuration
     config: Config,
 }
 
 impl HttpServer {
-    /// Creates a new `HttpServer` with given config.
+    /// Creates a new `HttpServer` using `config`.
     pub fn new(config: Config) -> Self {
         Self { config }
     }
 
-    /// Creates a `TcpListener`, listens on the host and port from the config
+    /// Creates a `TcpListener`, listens on the configured IP address and port
     /// and handles TCP requests.
-    ///
-    /// This method returns only if there is an error, otherwise
-    /// it listens infinitely.
     pub fn serve(&self) -> Result<(), Box<dyn Error>> {
         info!("creating a TCP listener");
 
@@ -52,14 +50,4 @@ impl HttpServer {
     fn make_listener(&self) -> io::Result<TcpListener> {
         TcpListener::bind((self.config.address, self.config.port))
     }
-
-    // Retrieves a TCP stream's body as a `String`.
-    // fn get_body(&self, stream: &mut TcpStream) -> io::Result<String> {
-    //     let mut buffer = [0; 2048];
-
-    //     match stream.read(&mut buffer) {
-    //         Err(e) => Err(e),
-    //         Ok(_) => Ok(String::from_utf8_lossy(&buffer).to_string()),
-    //     }
-    // }
 }
