3b6ba6df05dfbbf56027605c7682cb8b686337fb
karmek-k <33288445+karmek-k@users.noreply.github.com>
2025-12-27 17:49:39 +0100

    refactor(config): docstrings and tests

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,43 @@
 use std::net::{IpAddr, Ipv4Addr};
-use std::path::PathBuf;
 
-/// ksv server configuration.
-///
-/// This struct implements `Default` trait, by default the server
-/// should listen on `127.0.0.1` on port `8000`, and the root path
-/// should be the current working directory.
+/// Represents server configuration.
 #[derive(Debug)]
 pub struct Config {
+    /// IP address to listen on
     pub address: IpAddr,
+    /// TCP port number
     pub port: u16,
-    pub root: PathBuf,
 }
 
 impl Default for Config {
+    /// Provides a default Config.
+    ///
+    /// # Default values
+    /// * `address`: 127.0.0.1
+    /// * `port`: 8000
+    ///
+    /// # Example
+    /// ```
+    /// let config = Config::default();
+    /// assert_eq!(config.port, 8000);
+    /// ```
     fn default() -> Self {
         Self {
             address: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            port: 8000_u16,
-            root: PathBuf::from("."),
+            port: 8000,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config_default() {
+        let config = Config::default();
+
+        assert_eq!(config.port, 8000);
+        assert_eq!(config.address, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
     }
 }
