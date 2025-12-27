5a84d0d80a40b76e1f983ea2c9993bc10e73bed2
karmek-k <33288445+karmek-k@users.noreply.github.com>
2025-12-27 18:21:48 +0100

    refactor(status): remove comments

--- src/http/status.rs
+++ src/http/status.rs
@@ -1,32 +1,20 @@
-/// Status is an enum holding HTTP status identifiers.
-///
-/// In order to get a status code and message, use the `tuple` method.
+/// Represents a HTTP status identifier.
 #[allow(dead_code)]
 pub enum Status {
-    // Success
     Ok,
-
-    // Client-side Failure
     NotFound,
-
-    // Server-side Failure
     InternalServerError,
 }
 
+// TODO: change this implementation
 impl Status {
     /// Returns the status code and message of a `Status` enum instance.
     pub fn tuple(&self) -> (i32, &'static str) {
-        // less typing
         type S = Status;
 
         match self {
-            // Success
             S::Ok => (200, "OK"),
-
-            // Client-side Failure
             S::NotFound => (404, "Not Found"),
-
-            // Server-side Failure
             S::InternalServerError => (500, "Internal Server Error"),
         }
     }
