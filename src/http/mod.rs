b0cfab0edf7bf4663c328b81ab07f3628c1a01cc
karmek-k <33288445+karmek-k@users.noreply.github.com>
2025-12-27 17:51:59 +0100

    refactor(response): code and tests in one place

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,5 +2,3 @@ pub mod responder;
 pub mod response;
 pub mod server;
 pub mod status;
-
-mod response_test;
--- src/http/response.rs
+++ src/http/response.rs
@@ -35,3 +35,24 @@ impl<'a> Default for HttpResponse<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_string() {
+        let res = HttpResponse {
+            status: Status::Ok,
+            content_type: "text/plain",
+            body: String::from("Hello world"),
+        };
+
+        let res_string = res.to_string();
+
+        assert!(res_string.contains("HTTP"));
+        assert!(res_string.contains("200 OK"));
+        assert!(res_string.contains("Content-Type: text/plain"));
+        assert!(res_string.contains("Hello world"));
+    }
+}
